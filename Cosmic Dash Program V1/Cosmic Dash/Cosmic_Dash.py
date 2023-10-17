eae210bf0310c3ec757c0dc6d39694c230180b6d
Arnabsubedi233 <aaravsubedi1819@gmail.com>
2023-10-17 01:01:22 +0100

    Added new comments on menu system

--- Cosmic Dash Program V1/Cosmic Dash/Cosmic_Dash.py
+++ Cosmic Dash Program V1/Cosmic Dash/Cosmic_Dash.py
@@ -5,14 +5,17 @@ import game
 
 pygame.init()
 
+ # Import pygame.locals for easier access to key coordinates
+ # Updated to conform to flake8 and black standards
 from pygame.locals import (
         RLEACCEL
     )
-
+ # Define constants for the screen width and height
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 WHITE =  (255,255,255)
 
+#variables to hold buttons and images of buttons
 play_img = pygame.image.load("images/playbutton.png")
 play_button = buttons.Button(250,125,play_img,0.5)
 
@@ -24,17 +27,22 @@ exit_button = buttons.Button(250,375,exit_img,0.5)
 
 
 
-
+# Create the screen object
+# The size is determined by the constant SCREEN_WIDTH and SCREEN_HEIGHT
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+#flag for mainloop
 run = True
-
+#clock for better frames
 clock = pygame.time.Clock()
 
+#main loop
 while run == True:
+    #fills screen colour
     screen.fill((0, 0, 20))
     clock.tick(60)
 
+    #selection for if button press = true then executes command
     if play_button.draw(screen):
         game.game()
         run = False
@@ -45,8 +53,10 @@ while run == True:
 
         
 
-
+    #driver
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
                 run = False
+
+    #displays on screen
     pygame.display.flip()
